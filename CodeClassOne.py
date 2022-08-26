8622514d32bd49ded3a96b6ccabd6360be9b1fde
sethhi2 <sethcram@gmail.com>
2022-08-26 15:13:09 -0600

    everything including Webcam working

--- CodeClassOne.py
+++ CodeClassOne.py
@@ -1,34 +1,63 @@
 #!/usr/bin/env python3
-import os
 import cv2 as cv 
-print(cv.__version__)
+
+CLOSE_KEY = 'c'
+
+#read in image
+img = cv.imread('MeAndEmoly.jpg')
+
+def showImage(image, windowName = "OpenCV Picture", closeKey = 'c'):
+    """Shows the given image until the closeKey is pressed.
+
+    Args:
+        image (_type_): Image to show.
+        windowName (str, optional): Desired display window name. Defaults to "OpenCV Picture".
+        closeKey (str, optional): Key that needs to be pressed to close window. Defaults to 'c'.
+    """
+    #read 
+    key = ord('r')
+
+    #loop till close key pressed
+    while key != ord(closeKey):
+        cv.imshow(windowName, image)
+        key = cv.waitKey()
+    
+    #destroy windows
+    cv.destroyAllWindows()
+    
 
 #READ IN AND SHOW IMAGE
-"""
+
 #read in image
-img = cv.imread('/Users/sethhi2/Downloads/IMG_1448.jpg')
 key = ord('r')
-while key != ord('s'):
+
+while key != ord('c'):
     cv.imshow("OpenCV Picture", img)
     key = cv.waitKey()
 cv.destroyAllWindows()
-"""
+
 #GET A WEB CAM CAPTURE
 # already knows first webcam on mach to use
 
-#grab device 0
-"""
+#grab device 0 (TAKES A LONG TIME) (works on Windows)
 webcam = cv.VideoCapture(0)
 
-while key != ord('s'):
+#show webcam footage
+while (True):
     still = webcam.read()
     print(still)
     cv.imshow("OpenCV Webcam", still[1]) #first val is data type
-    key = cv.waitKey(10) #lower wait key val is laggier + less fps
+    
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv.waitKey(10) & 0xFF == ord('c'):
+        break
+    
+# After the loop release the cap object
+webcam.release()
 cv.destroyAllWindows()
-"""
-#read in image
-img = cv.imread('/Users/sethhi2/Downloads/Seth Professional Photo.png')
+
 # col (x), row (y), channel (z)
 #BGR Color Space
 # can access pixel using arr notation
@@ -39,7 +68,7 @@ print(img.dtype)
 #can index image
 img = img[250:500, 400:700]
 key = ord('r')
-while key != ord('s'):
+while key != ord('c'):
     cv.imshow("OpenCV Picture", img)
     key = cv.waitKey()
 cv.destroyAllWindows()
@@ -52,25 +81,25 @@ border_color = [255, 0, 0]
 # copy border pixels on edges to make new border image (REPLICATE = extending picels at end of border) ( or wrap to get pixels from other edge)
 img = cv.copyMakeBorder(img, border_size, border_size, border_size, border_size, cv.BORDER_CONSTANT, value = border_color) #border reflect (or reflect 101) (mirror pixels in)
 key = ord('r')
-while key != ord('s'):
+while key != ord('c'):
     cv.imshow("OpenCV Picture", img)
     key = cv.waitKey()
 cv.destroyAllWindows()
 
-#WRITE OUT AN IMAGE TO SAVE IT (can specify image type)
-path = os.getcwd()
-cv.imwrite(path, img) 
+#WRITE OUT AN IMAGE TO SAVE IT (need to specify image type)
+newFileName = "ZoomedInWithBorder_MeAndEmoly.jpg"
+cv.imwrite(newFileName, img) 
 
 #WRITE TEXT ON IMAGE
 # to classify something
 # for debugging if motion detected
-def write(img, text, origin = (50,50), font = cv.FONT_HERSHEY_SIMPLEX, fontScale = 1, color = (255,0,0), thickness = 2, line_type = cv.LINE_AA, botLeftOrigin = (0,0)):
-    newImg = cv.putText(img, text, origin, font, fontScale, color, thickness, line_type, botLeftOrigin )
+def writeText(img, text, origin = (50,50), font = cv.FONT_HERSHEY_SIMPLEX, fontScale = 1, color = (255,0,0), thickness = 2, line_type = cv.LINE_AA):
+    newImg = cv.putText(img, text, origin, font, fontScale, color, thickness, line_type)
     return newImg    
 
-write(img, "This is a picture")
+writeText(img, "This is a picture")
 key = ord('r')
-while key != ord('s'):
+while key != ord('c'):
     cv.imshow("OpenCV Picture", img)
     key = cv.waitKey()
 cv.destroyAllWindows()
