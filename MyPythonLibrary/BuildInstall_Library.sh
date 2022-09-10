1ea9a92502be7f52d575c048142e5d940d428762
Seth <sethcram@gmail.com>
2022-09-10 16:11:40 -0700

    bash build + install working

---
+++ MyPythonLibrary/BuildInstall_Library.sh
@@ -0,0 +1,3 @@
+python3 setup.py pytest
+python3 setup.py bdist_wheel
+pip3 install --force-reinstall /Users/sethhi2/source/CS404-MachineAndRoboticVision/MyPythonLibrary/dist/MachineVisionLibrary-0.1.1-py3-none-any.whl
--- MyPythonLibrary/build&install_library.bash
+++
@@ -1,3 +0,0 @@
-python3 setup.py pytest
-python3 setup.py bdist_wheel
-pip3 install --force-reinstall 
